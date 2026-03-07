7e6c6ebf0e709f3587bf7597ebc11e7038a477b3
tigerding <zhiyuanding01@gmail.com>
2026-03-07 12:18:23 -0500

    Update serialize impl

--- src/tasks/mod.rs
+++ src/tasks/mod.rs
@@ -1,7 +1,7 @@
 pub mod task_manager;
 
 use chrono::{DateTime, Duration, Local, Utc};
-use serde::ser::{Serialize, SerializeStruct, Serializer};
+use serde::{Deserialize, Serialize, Serializer};
 use std::sync::atomic::{self, AtomicU64};
 use tokio::sync::oneshot;
 
@@ -18,28 +18,21 @@ pub enum TaskCommand {
 }
 
 /// A task to be processed by the agent
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Task {
     pub id: TaskId,
     pub payload: String,
+
+    #[serde(serialize_with = "serialize_to_local")]
     pub deadline: DateTime<Utc>,
 }
 
-impl Serialize for Task {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let mut state = serializer.serialize_struct("Task", 3)?;
-        state.serialize_field("id", &self.id)?;
-        state.serialize_field("payload", &self.payload)?;
-
-        // Convert Utc to Local time for serialization
-        let local_deadline = self.deadline.with_timezone(&Local);
-        state.serialize_field("deadline", &local_deadline.to_rfc3339())?;
-
-        state.end()
-    }
+fn serialize_to_local<S>(date: &DateTime<Utc>, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    let local_date: DateTime<Local> = DateTime::from(*date);
+    serializer.serialize_str(&local_date.to_rfc3339())
 }
 
 impl Task {
